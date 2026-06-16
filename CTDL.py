dc171327ab91cdb2e0304cb23156527791367e84
PhuPA26 <phamanhphu05102006@gmail.com>
2026-06-16 16:33:52 +0700

    Add files via upload

---
+++ CTDL.py
@@ -0,0 +1,110 @@
+# Hash Node
+
+class HashNode:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+# Hash Map
+
+class HashMap:
+
+    # ---------- Public ----------
+
+    def __init__(self):
+        self.__TABLE_SIZE = 128
+        self.__buckets = [None] * self.__TABLE_SIZE
+
+    def insert(self, key, value):
+
+        index = self.__hash_function(key)
+
+        current = self.__buckets[index]
+
+        while current is not None:
+
+            if current.key == key:
+                current.value = value
+                return
+
+            current = current.next
+
+        new_node = HashNode(key, value)
+
+        new_node.next = self.__buckets[index]
+        self.__buckets[index] = new_node
+
+    def get(self, key):
+
+        index = self.__hash_function(key)
+
+        current = self.__buckets[index]
+
+        while current is not None:
+
+            if current.key == key:
+                return current.value
+
+            current = current.next
+
+        return None
+
+    def contains_key(self, key):
+        return self.get(key) is not None
+
+    def remove_key(self, key):
+
+        index = self.__hash_function(key)
+
+        current = self.__buckets[index]
+        previous = None
+
+        while current is not None:
+
+            if current.key == key:
+
+                if previous is None:
+                    self.__buckets[index] = current.next
+                else:
+                    previous.next = current.next
+
+                return
+
+            previous = current
+            current = current.next
+
+    def clear(self):
+
+        for i in range(self.__TABLE_SIZE):
+            self.__buckets[i] = None
+
+    def __setitem__(self, key, value):
+        self.insert(key, value)
+
+    def __getitem__(self, key):
+        value = self.get(key)
+        if value is None:
+            raise KeyError(key)
+        return value
+
+    def __contains__(self, key):
+        return self.contains_key(key)
+
+    def __delitem__(self, key):
+        self.remove_key(key)
+
+    # ---------- Private ----------
+
+    def __hash_function(self, key):
+
+        hash_value = 0
+
+        for ch in key:
+            hash_value = (
+                hash_value * 31 + ord(ch)
+            ) % self.__TABLE_SIZE
+
+        return hash_value
---
+++ model.py
@@ -0,0 +1,76 @@
+from CTDL import HashMap
+class Transaction:
+    def __init__(
+        self,
+        transaction_id,
+        amount,
+        date,
+        category_id,
+        transaction_type,
+        note=""
+    ):
+        self.transaction_id = transaction_id
+        self.amount = amount
+        self.date = date
+        self.category_id = category_id
+        self.transaction_type = transaction_type
+        self.note = note
+
+class Category:
+    def __init__(self, id, name, type, created_at=None, is_active=True, limit=0):
+        self.id = id
+        self.name = name
+        self.type = type  # "income" or "expense"
+        self.limit = limit  # Mặc định là 0
+        self.is_active = is_active # True nếu đang hoạt động, False nếu đã bị vô hiệu hóa
+    
+    def set_limit(self, new_limit):
+        self.limit = new_limit
+
+    def deactivate(self):
+        self.is_active = False
+
+    def activate(self):
+        self.is_active = True
+
+class CategoryState:
+    def __init__(self, category, year, month):
+        self.category = category
+        self.year = year
+        self.month = month
+
+class IncomeState(CategoryState):
+    def __init__(self, category, year, month):
+        super().__init__(category, year, month)
+        self.total_income = 0
+    def update_transaction(self, old_amount=0, new_amount=0, mode="add"):
+        if mode == "add":
+            self.total_income += new_amount
+
+        elif mode == "update":
+            self.total_income -= old_amount
+            self.total_income += new_amount
+
+        elif mode == "delete":
+            self.total_income -= old_amount
+
+class ExpenseState(CategoryState):
+    def __init__(self, category, year, month, limit):
+        super().__init__(category, year, month)
+        self.limit = category.limit
+        self.total_expense = 0
+    def update_transaction(self, old_amount=0, new_amount=0, mode="add"):
+        if mode == "add":
+            self.total_expense += new_amount
+
+        elif mode == "update":
+            self.total_expense -= old_amount
+            self.total_expense += new_amount
+
+        elif mode == "delete":
+            self.total_expense -= old_amount
+
+class MonthData:
+    def __init__(self):
+        self.category_states = HashMap()
+        self.transactions = []
